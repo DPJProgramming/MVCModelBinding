49c5c4e340b1783bc0f9db16526e2e167159c34f
David Jarvis <cheesyd64@hotmail.com>
2023-01-17 10:24:58 -0800

    first commit

---
+++ MVC ModelBinding/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ModelBinding.Models;
+
+namespace MVC_ModelBinding.Controllers {
+	public class MovieController : Controller {
+
+		
+		public IActionResult Index() {
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create() {
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Movie movieData) {
+			if(ModelState.IsValid) {
+
+				//add to database
+				return RedirectToAction("Index", "Home");
+			}
+			return View();
+		}
+	}
+}
---
+++ MVC ModelBinding/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MVC_ModelBinding.Models {
+	public class Movie {
+
+		/// <summary>
+		/// The movie title
+		/// </summary>
+		[Required(ErrorMessage = "Title Required")]
+		[Display(Name="title")]
+		public string Title {get; set;}
+
+		/// <summary>
+		/// The first and last name of the person who directed the movie
+		/// </summary>
+		[Required(ErrorMessage = "Who Directed This Movie?")]
+		[Display(Name = "director")]
+		public string Director { get; set;}
+
+		/// <summary>
+		/// the genre the movie falls under (science fiction, horror, ect)
+		/// </summary>
+		[Required(ErrorMessage = "Choose a genre")]
+		[Display(Name = "genre")]
+		public string Genre { get; set;}
+
+		/// <summary>
+		/// the cost to purchase the movie
+		/// </summary>
+		public double price { get; set; }
+
+	}
+}
